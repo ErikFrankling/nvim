ff84fc73a99eae36d4f90e985a893ac0621b4eb4
Erik Frankling <erik.frankling@frankling.se>
2025-03-19 15:19:55 +0100

    TeXpresso auto open

--- lua/custom/autocomd.lua
+++ lua/custom/autocomd.lua
@@ -11,3 +11,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  pattern = '*.tex',
+  command = 'TeXpresso %',
+})
